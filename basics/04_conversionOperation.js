c4bd980ae473dac7028c78a7597e7d8fecadfb9b
abhishek12lc <rahulsinghbtechs@gmail.com>
2025-03-26 17:43:20 +0000

    conversions

---
+++ basics/04_conversionOperation.js
@@ -0,0 +1,17 @@
+//"33"  => 33
+//"33abc" =>NaN
+//true=> 1 and false => 0
+//""=> false
+
+//"Abhishek" =>true
+
+let air="33"
+let air2=Number(air);
+console.log(air2);
+console.log(typeof air2);
+
+//number to string
+let number=33
+let number2=String(number);
+console.log(number2);
+console.log(typeof number2);
