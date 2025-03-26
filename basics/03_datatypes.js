636669848466e6a23ae0f7333663b65e29d4648c
abhishek12lc <rahulsinghbtechs@gmail.com>
2025-03-26 17:23:44 +0000

    datatypes

---
+++ basics/03_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //use all javascript code as newer version
+
+//alert (3+4)  //we are using nodejs, not browser
+
+//code readability should be high
+
+/*
+data types 
+1.number=> 2^53
+bitInt for big values
+2.string=>" cbc"
+3.boolean=>true or false
+4.null=>standalone values
+5.undefines => not given any value
+6.symbol=>for uniqueness
+7.object
+
+
+*/
+let age =24;
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const accountId=123344
+let accountEmail="abhishek@gmail.com"
+var accountPass="12345"
+accountCity="Jaipur"
+
+console.log([accountId, accountEmail, accountPass, accountCity]);
