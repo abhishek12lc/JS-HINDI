b0bd1cf45295f00cc7b65b6c44ee4b03b9efa3f2
abhishek12lc <rahulsinghbtechs@gmail.com>
2025-03-27 08:46:05 +0000

    strings

---
+++ basics/05_datatypes.js
@@ -0,0 +1,32 @@
+//DATATYPES
+
+// 1. Primitive => (7 types) [string, number, boolean, null, undefined, symbol, BigInt]
+
+/*const score=100
+const scoreValue= 100.3
+const isLoggedIn= true
+const OutsideTemp=null
+let useEmail;
+
+const  id=Symbol('123')
+const symbolId=Symbol('123')
+
+console.log(id === symbolId);
+*/
+
+
+// 2. Reference(Non- Primitive) => [array, objects, functions]
+
+
+
+
+
+
+//**********************************************************************************************************************//
+
+//STACK(primitive) AND HEAP(non-primitve) MEMORY IN JAVASCRIPT
+
+let myName="AbhishekKumarSingh"
+let anotherName=myName
+console.log(anotherName);
+
---
+++ basics/06_string.js
@@ -0,0 +1,11 @@
+//always use new syntax of string 
+
+let number1=12345
+let number2=67890
+
+console.log(`combination of number ${number1} ${number2}`);
+
+
+//(` `) => this is called back tick
+
+//go and read all the strings functions
